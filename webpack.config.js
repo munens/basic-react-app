277e9124dfa503fa8013610ef2240369933ca788
Munene Kaumbutho <kaumbuthom@yahoo.com>
2019-04-29 15:14:13 -0700

    basic-react-app: app runs on 8080

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  // entry point
+  entry: './src/index.js',
+
+   // output:
+   output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+
+  // loaders:
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
